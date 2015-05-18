73ea17d3222507c981345a3d27921e19254fa3e6
Paradox <parax_85@mail.ru>
2015-05-19 02:11:04 +0300

    66

--- notify.js
+++ notify.js
@@ -14,6 +14,7 @@ var server = http.createServer(function(request, response) {
 	var pathname = url.parse(request.url).pathname;
 	console.log("Request for " + pathname + " received.");
 	if (pathname == '/notify' && request.method == 'POST'){
+		console.log('here');
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		var query = url.parse(request.url, true).query;
 		if (query['room']!=null){
